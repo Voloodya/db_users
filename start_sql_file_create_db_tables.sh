a9613022b4dbf0185f402b2b2264e5df7c5ed487
vladimir <vladimir.s.rus@gmail.com>
2020-10-16 21:41:18 +0500

    First variable

--- start_sql_file_create_db_tables.sh
+++ start_sql_file_create_db_tables.sh
@@ -1,2 +1,2 @@
 echo "Start create db and tables"
-mysql -f -u root -p"123" < /sql_create_db_tables.sql
+mysql -f -u root -p"123" < /tmp/git/db_users/sql_create_db_tables.sql
--- start_sql_file_create_users_with_grant.sh
+++ start_sql_file_create_users_with_grant.sh
@@ -1,2 +1,2 @@
 echo "Start create users and grands"
-mysql -f -u root -p"123" < /sql_create_users_with_grant.sql
+mysql -f -u root -p"123" < /tmp/git/db_users/sql_create_users_with_grant.sql
