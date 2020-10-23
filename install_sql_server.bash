e09d84b530ca94ebe0e4e78efb3abb4602f0dfb1
voloodya <Vlad-i-mir.rus@yandex.ru>
2020-10-23 20:41:00 +0500

    Udate

--- install_sql_server.bash
+++ install_sql_server.bash
@@ -2,9 +2,11 @@
 cd /tmp
 echo "Download mysql"
 wget "https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb"
-echo "Unpaking mysql"
+echo "Unpacking mysql"
 sudo dpkg -i mysql-apt-config*
+echo "Updating repository configuration mysql"
 sudo dpkg-reconfigure mysql-apt-config
+echo "Updating index repository"
 sudo apt-get update
 echo "Install mysql-server mysql-client"
 apt-get install mysql-server mysql-client
