17981e2f127aa4c9d14ace684df133895aeed3a6
voloodya <Vlad-i-mir.rus@yandex.ru>
2020-10-30 18:47:27 +0500

    Udate

--- bd_Users.sql
+++ bd_Users.sql
@@ -1,40 +1,59 @@
 CREATE DATABASE  IF NOT EXISTS `db_users` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `db_users`;
 
-create table Users{
-	Id_Users int(11) NOT NULL AUTO_INCREMENT,
-	Login VARCHAR(50) NOT NULL,
-	Password VARCHAR(1000) NOT NULL,
-	World_coder VARCHAR(50) NOT NULL,
-	Status VARCHAR(25),
-	email VARCHAR(50) not null,
-	First_name VARCHAR(100) NOT NULL,
-	Last_name VARCHAR(100) NOT NULL
-}ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+Drop TABLE if exists user_roles;
 
-create table Roles{
-	Id_Roles int(11) NOT NULL AUTO_INCREMENT,
-	Description text NULL,
-}ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+DROP TABLE if exists users;
 
-create table GrantAuthority{
-	Id_GrantAuthority int(11) NOT NULL AUTO_INCREMENT,
-	User_id int(11) NOT NULL,
-	CONSTRAINT FK_GrantAuthority_Users FOREIGN KEY (User_id) REFERENCES Users (Id_Users)
-	ON DELETE CASCADE,
-	Role_id int(11) NOT NULL,
-	CONSTRAINT FK_GrantAuthority_Roles FOREIGN KEY (Role_id) REFERENCES Roles (Id_Roles)
-	ON DELETE CASCADE, 
-}ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+Drop TABLE if exists data_user;
 
-create table DataUser{
-	Id_GrantAuthority int(11) NOT NULL AUTO_INCREMENT,
-	User_id int(11) NOT NULL,
-	CONSTRAINT FK_GrantAuthority_Users FOREIGN KEY (User_id) REFERENCES Users (Id_Users)
-	ON DELETE CASCADE,
-	Full_name_company VARCHAR(256) NULL,
-	Short_name_company VARCHAR(256) NULL,
-	First_name VARCHAR(100) NOT NULL,
-	Patronymic_name VARCHAR(100) NULL,
-	Family_name VARCHAR(100) not null	 
-}ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+drop table if exists roles;
+
+create table Data_user(
+                          Id_data_user BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          Full_name_company VARCHAR(256) NULL,
+                          Short_name_company VARCHAR(256) NULL,
+                          Name_director VARCHAR(100) NULL,
+                          Patronymic_director VARCHAR(100) NULL,
+                          Family_name_director VARCHAR(100) null,
+                          Telefone VARCHAR(12)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+create table Users(
+                      Id_Users BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      Login VARCHAR(50) NOT NULL,
+                      Password VARCHAR(1000) NOT NULL,
+                      Word_coder VARCHAR(50) NOT NULL,
+                      Status VARCHAR(25),
+                      email VARCHAR(256) not null,
+                      First_name VARCHAR(100) NOT NULL,
+                      Last_name VARCHAR(100) NOT NULL,
+                      Created DateTime NOT NULL,
+                      Updated DateTime NOT NULL,
+                      Data_user BIGINT Not NULL,
+                      Data_user_id BIGINT default null,
+                      CONSTRAINT FK_Users_Data_user FOREIGN KEY (Data_user_id) REFERENCES Data_user (Id_data_user)
+                      ON delete set null on update restrict
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create table Roles(
+                      Id_Roles INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      Name VARCHAR(100) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create table User_roles(
+                           Id_User_roles BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                           User_id BIGINT NOT NULL,
+                           CONSTRAINT FK_User_roles_Users FOREIGN KEY (User_id) REFERENCES Users (Id_Users)
+                               ON DELETE CASCADE,
+                           Role_id INT NOT NULL,
+                           CONSTRAINT FK_User_roles_Roles FOREIGN KEY (Role_id) REFERENCES Roles (Id_Roles)
+                               ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO Users (Id_Users, Login, Password, Word_coder, Status, email, First_name, Last_name, Created, Updated) VALUES (1,'vldmr','42f846961c6718e804bcdd0f04d32a9d44fa3a4a51d4983026bd8b31525af5b5',
+                     'vldmr','ACTION','vldmr@gmail.com','Vladimir','S','2020.10.25','2020.10.25');
+
+INSERT INTO Roles VALUES (1,'ROLE_ADMIN');
+INSERT INTO Roles VALUES (2,'ROLE_USER');
+
+INSERT INTO User_roles VALUES (1,1,1);
--- sql_create_db_tables.sql
+++ sql_create_db_tables.sql
@@ -1,44 +1,46 @@
 DROP DATABASE IF EXISTS db_users;
-CREATE DATABASE IF NOT EXISTS  db_users;
+CREATE DATABASE IF NOT EXISTS  db_users /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE db_users;
-create table Users(
-	Id_Users BIGINT NOT NULL AUTO_INCREMENT,
-	Login VARCHAR(50) NOT NULL,
-	Password VARCHAR(1000) NOT NULL,
-	World_coder VARCHAR(50) NOT NULL,
-	Status VARCHAR(25),
-	email VARCHAR(256) not null,
-	First_name VARCHAR(100) NOT NULL,
-	Last_name VARCHAR(100) NOT NULL,
-	Created DateTime NOT NULL,
-	Updated DateTime NOT NULL,
-	PRIMARY KEY (Id_Users)
-);
-create table Roles(
-	Id_Roles BIGINT NOT NULL AUTO_INCREMENT,
-	Name VARCHAR(100) NOT NULL,
-	PRIMARY KEY (Id_Roles)
-);
-create table User_roles(
-	Id_User_roles BIGINT NOT NULL AUTO_INCREMENT,
-	User_id BIGINT NOT NULL,
-	CONSTRAINT FK_User_roles_Users FOREIGN KEY (User_id) REFERENCES Users (Id_Users)
-	ON DELETE CASCADE,
-	Role_id BIGINT NOT NULL,
-	CONSTRAINT FK_User_roles_Roles FOREIGN KEY (Role_id) REFERENCES Roles (Id_Roles)
-	ON DELETE CASCADE,
-	PRIMARY KEY (Id_User_roles)
-);
 create table Data_user(
-	Id_Data_user BIGINT NOT NULL AUTO_INCREMENT,
-	User_id BIGINT NOT NULL,
-	CONSTRAINT FK_Data_user_Users FOREIGN KEY (User_id) REFERENCES Users (Id_Users)
-	ON DELETE CASCADE,
-	Full_name_company VARCHAR(256) NULL,
-	Short_name_company VARCHAR(256) NULL,
-	Name_director VARCHAR(100) NULL,
-	Patronymic_director VARCHAR(100) NULL,
-	Family_name_director VARCHAR(100) null,
-	Telefone VARCHAR(12),
-	PRIMARY KEY (Id_Data_user)	
-);
+                          Id_data_user BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          INN VARCHAR(12) NOT NULL,
+                          Full_name_company VARCHAR(256) NULL,
+                          Short_name_company VARCHAR(256) NULL,
+                          Name_director VARCHAR(100) NULL,
+                          Patronymic_director VARCHAR(100) NULL,
+                          Family_name_director VARCHAR(100) null,
+                          Telefone VARCHAR(12) null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create table Users(
+                      Id_users BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      Login VARCHAR(50) NOT NULL,
+                      Password VARCHAR(1000) NOT NULL,
+                      Word_coder VARCHAR(50) NOT NULL,
+                      Status VARCHAR(25),
+                      Email VARCHAR(256) not null,
+                      First_name VARCHAR(100) NOT NULL,
+                      Last_name VARCHAR(100) NOT NULL,
+                      Created_date DateTime NOT NULL,
+                      Updated_date DateTime NOT NULL,
+                      Data_user_id BIGINT default null,
+                      CONSTRAINT FK_Users_Data_user FOREIGN KEY (Data_user_id) REFERENCES Data_user (Id_data_user)
+                          ON delete set null on update restrict
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create table Roles(
+                      Id_roles INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      Name VARCHAR(100) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+create table User_roles(
+                           Id_user_roles BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                           User_id BIGINT NOT NULL,
+                           CONSTRAINT FK_User_roles_Users FOREIGN KEY (User_id) REFERENCES Users (Id_users)
+                               ON DELETE CASCADE,
+                           Role_id INT NOT NULL,
+                           CONSTRAINT FK_User_roles_Roles FOREIGN KEY (Role_id) REFERENCES Roles (Id_roles)
+                               ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO Users (Id_Users, Login, Password, Word_coder, Status, email, First_name, Last_name, Created_date, Updated_date) VALUES (1,'vldmr','42f846961c6718e804bcdd0f04d32a9d44fa3a4a51d4983026bd8b31525af5b5','vldmr','ACTION','vldmr@gmail.com','Vladimir','S','2020.10.25','2020.10.25');
+INSERT INTO Roles VALUES (1,'ROLE_ADMIN');
+INSERT INTO Roles VALUES (2,'ROLE_USER');
+INSERT INTO User_roles VALUES (1,1,1);
