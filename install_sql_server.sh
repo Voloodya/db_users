21a19cf7c7bb89dc6b0cd5d1964ce162a037cf8f
voloodya <Vlad-i-mir.rus@yandex.ru>
2020-11-06 18:16:53 +0500

    First variable

--- install_sql_server.sh
+++ install_sql_server.sh
@@ -1,10 +1,16 @@
-
 cd /tmp
 echo "Download mysql"
 wget "https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb"
-echo "Anpaking mysql"
+echo "Unpacking mysql"
 sudo dpkg -i mysql-apt-config*
+echo "Updating repository configuration mysql"
 sudo dpkg-reconfigure mysql-apt-config
+echo "Updating index repository"
 sudo apt-get update
-echo "Install mysql-server mysql-client"
-apt-get install mysql-server mysql-client
+echo "--------------------------------------------------------------"
+echo "Install mysql-server"
+apt-get install mysql-server
+echo "Install mysql-client"
+apt-get install mysql-client
+echo "Test service mysql"
+sudo systemctl status mysql
